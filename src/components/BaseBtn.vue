649a0175a76a20d3de12eb6b1c24585375ef323c
Sergey Loginov <the.loginov@gmail.com>
2019-03-20 11:44:38 +0500

    Added components: TheFilter, BaseBtn, BaseInp

---
+++ src/components/BaseBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-button class="BaseBtn" size="sm" variant="primary">
+    <slot></slot>
+  </b-button>
+</template>
+
+<script>
+  export default {
+  name: 'BaseBtn'}
+</script>
+
+<style scoped>
+  .BaseBtn {
+    margin: 10px;
+}
+</style>
---
+++ src/components/BaseInp.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <b-form-input size="sm" type="text"></b-form-input>
+  </div>
+</template>
+
+
+<script scoped>
+  export default {
+  name: 'BaseInput',
+  props: {
+//    val: String
+  },  
+  data() {
+      return {
+    // val: 'ddfd'
+    toParent: 'ddd1'
+}
+  }
+  }
+</script>
+
+
+<style>
+</style>
